07a37609fa97e2076f5c03f5fb6c7c5ec8d582ed
ALexd-78 <alexd-78@yandex.ru>
2023-01-27 01:29:57 +0300

    добавлена функция go_title_str в str_func.py добавлены докстринги

--- str_func.py
+++ str_func.py
@@ -7,4 +7,12 @@ def go_upper_str():
     upper_str = user_input.upper()
     return upper_str
 
-#print(go_upper_str())
+
+def go_title_str():
+    """
+    делает заглавными первые буквы каждого
+    слова в строке, поступившей на вход
+    """
+    user_input = input("Введите строку")
+    title_str = user_input.title()
+    return title_str
